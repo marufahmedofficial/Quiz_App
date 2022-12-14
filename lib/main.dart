33ae17bbca2b476bf050c438896a8a09a69ad17d
Maruf Ahmed <mk6116055@gmail.com>
2022-12-15 02:27:49 +0600

    homepage created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
+      home: HomePage(),
     );
   }
 }
